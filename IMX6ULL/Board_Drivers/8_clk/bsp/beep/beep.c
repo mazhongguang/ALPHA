f37715433f3b17c5372d7d8fb467562e976c5810
mazhongguang <mazhongguang@163.com>
2019-11-16 16:02:16 +0800

    CLK时钟树相关配置实验完成

---
+++ IMX6ULL/Board_Drivers/8_clk/bsp/beep/beep.c
@@ -0,0 +1,24 @@
+#include "beep.h"
+#include "cc.h"
+#include "gpio.h"
+/* BEEP初始化 */
+void Beep_Init(void)
+{
+	gpio_pin_config_t beep_config;
+	IOMUXC_SetPinMux(IOMUXC_SNVS_SNVS_TAMPER1_GPIO5_IO01, 0);	//复用为GIPO5_IO01
+	IOMUXC_SetPinConfig(IOMUXC_SNVS_SNVS_TAMPER1_GPIO5_IO01, 0X10B0);
+
+	/* GPIO初始化 */
+	beep_config.direction = kGPIO_DigitalOutput;
+	beep_config.outputLogic = 1;
+	gpio_init(GPIO5, 1, &beep_config);
+}
+
+/* 蜂鸣器控制函数 */
+void beep_switch(int status)
+{
+	if(status ==ON)
+		gpio_pinwrite(GPIO5, 1, 0);
+	else if (status == OFF)
+		gpio_pinwrite(GPIO5, 1, 1);
+}
---
+++ IMX6ULL/Board_Drivers/8_clk/project/main.h
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "fsl_common.h"
+#include "fsl_iomuxc.h"
+#include "MCIMX6Y2.h"
+
+#endif // !__MAIN_H
