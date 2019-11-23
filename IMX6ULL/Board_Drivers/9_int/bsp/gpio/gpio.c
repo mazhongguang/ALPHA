ca8544693f5a5ea3c72de1efe7ee0592e22b9db5
mazhongguang <mazhongguang@163.com>
2019-11-23 16:00:12 +0800

    中断实验完成

--- IMX6ULL/Board_Drivers/9_int/bsp/gpio/gpio.c
+++ IMX6ULL/Board_Drivers/9_int/bsp/gpio/gpio.c
@@ -13,6 +13,7 @@ void gpio_init(GPIO_Type *base, int pin, gpio_pin_config_t *config)
 			/* 设置默认输出电平 */
 			gpio_pinwrite(base, pin, config->outputLogic);
 		}
+	gpio_intconfig(base, pin, config->interruptMode);
 }
 
 /* 控制GPIO高低电平 */
@@ -32,4 +33,66 @@ void gpio_pinwrite(GPIO_Type *base, int pin, int value)
 int gpio_pinread(GPIO_Type *base, int pin)
 {
 	return (((base->DR) >> pin) & 0x1);
+}
+
+/*使能指定IO中断 */
+void gpio_enableint(GPIO_Type *base, unsigned int pin)
+{
+	base->IMR |= (1 << pin);
+}
+
+/* 禁止指定IO中断 */
+void gpio_disableint(GPIO_Type *base, unsigned int pin)
+{
+	base->IMR &= ~(1 << pin);
+}
+
+/* 清除中断标志位 */
+void gpio_clearintflags(GPIO_Type *base, unsigned int pin)
+{
+	base->ISR |= (1 << pin);
+}
+
+/* GPIO中断初始化函数 */
+void gpio_intconfig (GPIO_Type *base, unsigned int pin,
+                gpio_interrupt_mode_t pin_int_mode)
+{
+  volatile uint32_t *icr;
+  uint32_t icrShift;
+
+  icrShift = pin;
+  base->EDGE_SEL &= ~(1 << pin);
+
+  if (pin < 16) /* 低16位 */
+    {
+      icr = &(base->ICR1);
+    }
+  else
+    {
+      icr = &(base->ICR2);
+      icrShift -= 16;
+    }
+  switch (pin_int_mode)
+    {
+		case kGPIO_IntLowLevel:
+			*icr &= ~(3 << (2 * icrShift));
+			break;
+		case	kGPIO_IntHighLevel:
+				*icr &= ~(3 << (2 * icrShift));
+				*icr |= (1 << (2 * icrShift));
+				break;
+		case	kGPIO_IntRisingEdge:
+				*icr &= ~(3 << (2 * icrShift));
+				*icr |= (2 << (2 * icrShift));
+				break;
+		case	kGPIO_IntFallingEdge:
+				*icr &= ~(3 << (2 * icrShift));
+				*icr |= (3 << (2 * icrShift));
+				break;
+		case	kGPIO_IntRisingOrFallingEdge:
+				base->EDGE_SEL |= (1 << pin);
+				break;
+		default:
+			break;
+    }
 }
